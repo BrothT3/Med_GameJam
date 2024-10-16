b779d7100cfee53eb8e3ee194ce842c4824f81ef
BrothT3 <79831328+BrothT3@users.noreply.github.com>
2024-10-16 16:18:37 +0200

    TempFix

---
+++ Assets/Scripts/States/MenuState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class MenuState : State
+{
+    public override void End()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Execute()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Init()
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ Assets/Scripts/States/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public abstract class State : MonoBehaviour
+{
+    public abstract void Init();
+    public abstract void Execute();
+    public abstract void End();
+}
+
